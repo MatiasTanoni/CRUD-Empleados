ca1f70f65421528e6f100df63569bc4b11a7568b
MatiasTanoni <matitanoni@gmail.com>
2024-07-02 22:40:39 -0300

    diagrama de clases

--- FormularioLogin/obj/Debug/net6.0-windows/Formularios.AssemblyInfo.cs
+++ FormularioLogin/obj/Debug/net6.0-windows/Formularios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Formularios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f223f3824b3b3b0151c1a4f344f55380520b4e89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff7c22ecc5b06b7943c370db90cc480f03d007d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Formularios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Formularios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
